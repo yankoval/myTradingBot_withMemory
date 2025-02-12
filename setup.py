91c180102103ddb96ee7eba34034ea80a8831dc4
ivan <yankoval@gmail.com>
2025-02-12 16:34:06 +0300

    Data3 for bay strategy with diff fractals precalculated read csv cvargs removed

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='myTradingBot_withMemory',
-    version='',
+    version='1.0',
     packages=['qbroker', 'trading_bot'],
     url='',
     license='',
     author='ivan',
-    author_email='',
-    description=''
+    author_email='yankoval@gmail.com',
+    description='trading bot q-learning'
 )
